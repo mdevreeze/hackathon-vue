0012c3fa0c4ff3c32863195256a1c768673116be
Ruben Duiveman <ruben@uptrends.com>
2019-11-08 16:58:16 +0100

    ESLint update.

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'indent': ["off", 4],
+    'space-before-function-paren': ["off", true]
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
